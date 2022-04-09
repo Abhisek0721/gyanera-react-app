4dd4085d5ac15dbcd6a1b37d5b68baa2073bd162
Abhisekh Upadhaya <abhisekh@localhost.localdomain>
2022-04-09 17:58:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home category={"programming"}/>} exact />
+          <Route path="programming" element={<Home category={"programming"} />} exact />
+          <Route path="business" element={<Home category={"business"} />} exact />
+          <Route path="marketing" element={<Home category={"marketing"} />} exact />
+          <Route path="design" element={<Home category={"design"} />} exact />
+          <Route path="photography" element={<Home category={"photography"} />} exact />
+          <Route path="music" element={<Home category={"music"} />} exact />
+          <Route path="personal-dev" element={<Home category={"personal-dev"} />} exact />
+          <Route path='about' element={<About />} exact />
+          <Route path='contact' element={<Contact />} exact />
+          <Route path='blog' element={<Blog />} exact />
+          <Route path='login' element={<Login />} exact />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
